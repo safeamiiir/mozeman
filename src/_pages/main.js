3e417907342af36edc49c9f5efbd4fcf851b715f
safeamiiir <safehian_amirreza@yahoo.com>
2020-03-23 05:49:30 +0430

    handle add new background color

--- src/_pages/main.js
+++ src/_pages/main.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 import Draggable from "../_components/draggable";
 
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #6bfeba 30%, #FFC107 90%)",
+    height: "100vh",
+    overflow: "hidden"
+  }
+});
+
 function MainPage() {
+  const classes = useStyles();
   return (
-    <Grid className="App">
-      <Draggable position={{ x: 250, y: 250 }} radius={38}/>
+    <Grid className={classes.root}>
+      <Draggable position={{ x: 250, y: 250 }} radius={38} />
     </Grid>
   );
 }
